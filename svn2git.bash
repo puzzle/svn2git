0d7083e08c82d53ac216ba1d7275e6f133cfd6af
Stefan Rotman <rotman@puzzle.ch>
2016-05-13 14:17:42 +0200

    Add argument flags and usage help

--- svn2git.bash
+++ svn2git.bash
@@ -1,25 +1,55 @@
-#!/bin/bash -x
-: ${SVN_REPOSITORY:?"SVN_REPOSITORY is required"}
-: ${PROJECT:?"PROJECT is required"}
-: ${GIT_REPOSITORY:?"GIT_REPOSITORY is required"}
+#!/bin/bash
 
-: ${WORKDIR:=$(mktemp -d -t svn2git."$PROJECT".XXXXX)}
-: ${AUTHORS_FILE:="$WORKDIR/authors"}
-: ${MIGRATE_DIR:="$WORKDIR/migration"}
-: ${BARE_DIR:="$WORKDIR/${PROJECT}-bare.git"}
+function usage {
+    cat << EOF
+Usage: $0 <parameters>
 
-: ${AUTHORS_PATTERN:='$1 <$1>'}
+Required parameters:
 
-: ${GIT_AUTHOR_FLAG:='--author="SVN 2 Git Migration <svn2git>"'}
+    -p <projectname>
+    --project=<projectname>
+	Sets the name of the project to migrate.
+        This becomes optional if the 'PROJECT" environment variable has been set.
+
+    -s <svn repository>
+    --svn=<svn repository>
+	Sets the Subversion Repository URL. Important is that this points to the root of the Subversion
+        repository, so trunk, tags and branches are accessible
+        This becomes optional if the 'SVN_REPOSITORY" environment variable has been set.
+
+    -g <git repository>
+    --git=<sgit repository>
+	Sets the Git Repository URL. This repository should exist and be empty.
+        This becomes optional if the 'GIT_REPOSITORY" environment variable has been set.
+
+    Example:
+        $ $0 -p myproject -s svn://myproject.code.svn -g ssh://git@myproject.code.git
+            Migrates 'myproject' from 'svn://myproject.code.svn' to 'ssh://git@myproject.code.git'
+
+Optional parameters:
+
+    --no-metadata
+        Don't keep the reference to the original SVN revision in the migrated Git repository
+
+    -v
+    --verbose
+        Run in verbose mode
+
+    -h
+    --help
+        Print this message and exit
+
+EOF
+   exit
+}
 
 function createAuthorsFile {
     svn log --xml "$SVN_REPOSITORY" | grep author | perl -pe "s/.*>(.*?)<.*/\$1 = $AUTHORS_PATTERN/" | sort -u > "$AUTHORS_FILE"
 }
 
 function checkoutGitSVN {
-    #TODO sr - add flag for skipping metadata
     #TODO sr - add flags for non-standard layouts
-    git svn clone "$SVN_REPOSITORY" --stdlayout --prefix=svn/ --authors-file="$AUTHORS_FILE" -s "$MIGRATE_DIR"
+    git svn clone "$SVN_REPOSITORY" $NO_METADATA_FLAG --stdlayout --prefix=svn/ --authors-file="$AUTHORS_FILE" -s "$MIGRATE_DIR"
 }
 
 function migrateTags {
@@ -44,7 +74,7 @@ function migrateBranches {
 function migrateIgnoredFiles {
     cd "$MIGRATE_DIR"
     git svn create-ignore
-    git commit $GIT_AUTHOR_FLAG -m "Migrate svn:ignore to .gitignore" .gitignore
+    git commit --author="SVN 2 Git Migration <svn2git>" -m "Migrate svn:ignore to .gitignore" .gitignore
 
 }
 
@@ -59,6 +89,68 @@ function pushBareClone {
     git push gitremote --tags
 }
 
+params="$(getopt -o p:sghv -l project:,svn,git,help,verbose,no-metadata --name "$(basename -- "$0")" -- "$@")"
+if [ $? -ne 0 ]
+then
+    usage
+fi
+
+eval set -- "$params"
+unset params
+
+while true
+do
+    case $1 in
+        -p|--project)
+          PROJECT=$2
+          shift 2
+          ;;
+        -s|--svn)
+          SVN_REPOSITORY=$2
+          shift 2
+          ;;
+        -g|--git)
+          GIT_REPOSITORY=$2
+          shift 2
+          ;;
+        -h|--help)
+	  usage
+          shift
+          ;;
+        -v|--verbose)
+	  VERBOSE="VERBOSE"
+          shift
+          ;;
+        --no-metadata)
+	  NO_METADATA_FLAG="--no-metadata"
+          shift
+          ;;
+        --)
+          shift
+          break
+          ;;
+        *)
+          usage
+          ;;
+    esac
+done
+
+if [ "$VERBOSE" == "VERBOSE" ] ; then set -x ; fi
+
+
+: ${PROJECT:?"PROJECT is required"}
+: ${SVN_REPOSITORY:?"SVN_REPOSITORY is required"}
+: ${GIT_REPOSITORY:?"GIT_REPOSITORY is required"}
+
+: ${WORKDIR:=$(mktemp -d -t svn2git."$PROJECT".XXXXX)}
+: ${AUTHORS_FILE:="$WORKDIR/authors"}
+: ${MIGRATE_DIR:="$WORKDIR/migration"}
+: ${BARE_DIR:="$WORKDIR/${PROJECT}-bare.git"}
+
+: ${AUTHORS_PATTERN:='$1 <$1>'}
+: ${NO_METADATA_FLAG:=""}
+
+
 createAuthorsFile
 checkoutGitSVN
 migrateTags
@@ -66,3 +158,5 @@ migrateBranches
 migrateIgnoredFiles
 cloneBare
 pushBareClone
+
+#TODO sr cleanup
